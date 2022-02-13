5d10e2dd1242a1e4877f81f88d37ba7609576cba
blazotadic <95979502+blazotadic@users.noreply.github.com>
2022-02-13 23:03:11 +0100

    Add files via upload

---
+++ logate-academy-angular-2022-master/logate-academy-angular-2022-master/src/app/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component } from "@angular/core";
+import { NavbarLink } from "./navbar.model";
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+
+  isMobileViewActive: boolean = false;
+
+  links: NavbarLink[] = [
+    {
+      name: "Home",
+      path: "/home"
+    },
+    {
+      name: "Student",
+      path: "/student"
+    },
+    {
+      name: "Playground",
+      path: '/playground'
+    },
+    {
+      name: "Contact",
+      path: "/contact"
+    }
+  ];
+
+  toggleMobileView(): void {
+    this.isMobileViewActive = !this.isMobileViewActive;
+  }
+}
